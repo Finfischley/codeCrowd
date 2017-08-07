f00046037543ebc8e9b7b030d23fb07b935d58dd
jazmin10 <jazminestrada10@gmail.com>
2017-08-06 19:27:44 -0500

    adding users api routes

--- models/interview-prep.js
+++ models/interview-prep.js
@@ -18,7 +18,7 @@ module.exports = function(sequelize, DataTypes){
 		state: {
 			type: DataTypes.STRING,
 			allowNull: false
-		}
+		},
 		position: {
 			type: DataTypes.STRING,
 			allowNull: false
---
+++ routes/user-api-routes.js
@@ -0,0 +1,45 @@
+// Users Routes =====================================================
+
+// Import models
+var db = require("../models");
+
+module.exports = function(app){
+
+	// READ user information to check if they can log in
+	app.get("/:username/:password", function(readReq, readRes){
+
+		// console.log(readReq.params.username);
+		db.User.findAll({
+			where: {
+				username: readReq.params.username
+			}
+		}).then(function(results){
+			// console.log(results[0]);
+			if (!results[0]){
+				readRes.send("wrong username");
+				return;
+			}
+			var password = results[0].password;
+
+			if (readReq.params.password === password){
+				readRes.send("you are logged in");
+				loggedIn = true;
+				// console.log(loggedIn);
+				return;
+			}
+
+			readRes.send("wrong password");
+
+		})
+	})
+
+	// CREATE new user
+	app.post("/newuser", function(createReq, createRes){
+
+		db.User.create(createReq.body).then(function(results){
+			createRes.end();
+		});
+	});
+
+}
+
